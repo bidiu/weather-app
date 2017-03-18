7537ff85cb1ffd8d4b65550a47683e5738db1987
bedew <sunhe1007@126.com>
2017-03-18 17:07:42 -0400

    add webpack clean build script

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,8 +1,37 @@
 var webpack = require("webpack");
 var path = require("path");
+var fs = require('fs');
 
 var DEV = path.resolve(__dirname, "dev");
 var OUTPUT = path.resolve(__dirname, "public");
+
+// TODO make it a webpack plguin
+// based on https://gist.github.com/liangzan/807712, altered
+function cleanDir(dirPath, opt) {
+  if (opt.ignore === undefined) opt.ignore = [];
+  if (opt.dry === undefined) opt.dry = false;
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { console.log("Custom clean script: \n" + e); return; }
+
+  if (files.length > 0) {
+    for (var i = 0; i < files.length; i++) {
+      var fileName = files[i];
+      var filePath = dirPath + '/' + fileName;
+      if (fs.statSync(filePath).isFile() && !opt.ignore.includes(fileName)) {
+        console.log("Custom clean script: going to delete " + filePath);
+        if (!opt.dry) fs.unlinkSync(filePath)
+      } else if (! fs.statSync(filePath).isFile()) {
+        cleanDir(filePath, opt);
+      }
+    }
+  }
+}
+
+// clean previous build - custom script
+cleanDir(OUTPUT, {
+  ignore: ['.gitkeep'],
+  dry: false
+});
 
 var config = {
   devtool: 'source-map',
