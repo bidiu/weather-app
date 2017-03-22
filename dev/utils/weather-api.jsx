9db3db3d79880f3973b55f4739febf02c4c336aa
bedew <sunhe1007@126.com>
2017-03-22 00:17:03 -0400

    prefix weather api endpoint

--- dev/utils/weather-api.jsx
+++ dev/utils/weather-api.jsx
@@ -2,4 +2,4 @@ const CONFIG = require('../../config');
 
 export const API_KEY = CONFIG.apiKey;
 
-export const CUR_WEATHER_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather";
+export const CUR_WEATHER_ENDPOINT = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather";
